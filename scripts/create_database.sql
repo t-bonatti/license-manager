face0e62c235c477a12ff024b90e99c3649ec43f
t-bonatti <tiago.bonatti@gmail.com>
2017-07-13 21:51:32 -0300

    Fix script

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -4,6 +4,5 @@ CREATE TABLE licenses
   version character varying(10),
   created_at timestamp without time zone NOT NULL,
   info json,
-  CONSTRAINT licenses_pkey PRIMARY KEY (id),
   CONSTRAINT uk_id_version UNIQUE (id, version)
 )
